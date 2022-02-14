fefeb0c001dbc17315cc9e0587ad246dfd1efc9f
Will W <wjwat@onionslice.org>
2022-02-14 12:11:03 -0600

    Fix bug in page selection in sidebar
    Co-Authored-by: Ryan B. <theryanbass@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './css/site.css';
 import './modules/home/home.js';
 import './modules/example/example.js';
 import './modules/badexample/badexample.js';
+import './modules/quotes/quotes.js';
 
 // Highlight which page we're currently on in the navigation sidebar.
 $(() => {
@@ -12,5 +13,4 @@ $(() => {
     $('.navigation a').removeClass('active');
     $(e.target).addClass('active');
   });
-  $('.navigation a[href="home/"]').addClass('active');
 });
