a84e04c8e88ce5a143ceb5da9e70facf9b8e940a
Will W <wjwat@onionslice.org>
2022-02-14 12:11:27 -0600

    WIP quotes functionality
    Co-Authored-by: Ryan B. <theryanbass@gmail.com>

--- src/modules/quotes/quotes.js
+++ src/modules/quotes/quotes.js
@@ -1,6 +1,30 @@
 import $ from 'jquery';
 import './quotes.css';
 
+async function fetchQuote() {
+  const response = await fetch('https://programming-quotes-api.herokuapp.com/Quotes/Random');
+  // const result = await response.json();
+
+  return response.json();
+  // return fetch(`https://programming-quotes-api.herokuapp.com/Quotes/Random`)
+  //   .then(function(res) {
+  //     if (!res) {
+  //       throw Error (res.statusText);
+  //     }
+  //     return res.json();
+  //   })
+  //   .catch(function(error) {
+  //     return error;
+  //   });
+} 
+
+// transform quote
+
+
 $(() => {
-  // do something
+  fetchQuote()
+    .then(resp => {
+      console.log(resp);
+      $('.quotes #output').text(resp.en + ' -- ' + resp.author);
+    });
 });
