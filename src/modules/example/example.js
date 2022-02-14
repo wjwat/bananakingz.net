ac61abf2b5fbc8e0106b6e3109f37c3a1b9d1df1
Will W <wjwat@onionslice.org>
2022-02-14 07:35:06 -0600

    Add examples for other devs

--- src/modules/example/example.js
+++ src/modules/example/example.js
@@ -1,1 +1,9 @@
+import $ from 'jquery';
 import './example.css';
+
+// We wait for the page to load, select our namedspaced h2, and change the text
+// of it to something else. This is a good example.
+$(() => {
+  $('.example h2').html('Hello! This is a good example!');
+  $('.example h3').html('Let me live');
+});
