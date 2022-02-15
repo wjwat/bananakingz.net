9dbb427a292efedef68495ad5a003405b1af0f3d
Will W <wjwat@onionslice.org>
2022-02-15 12:00:18 -0600

    Add UI to jokes & finish functionality
    Co-authored-by: Alex Adamovic <alexanderadamovic@gmail.com>

--- src/modules/jokes/jokes.js
+++ src/modules/jokes/jokes.js
@@ -4,6 +4,21 @@ import './jokes.css';
 
 const fetchJoke = async () => {
   const response = await fetch('https://wowthatsbig.net/api/randomjoke');
-
   return response.json();
 };
+
+export default function displayJoke() {
+  fetchJoke()
+    .then (resp => {
+      const randomJoke = resp[0];
+      $('.jokes #output').text('🤣😂\n' + randomJoke + '\n🤣😂');
+    });
+}
+
+$(() => {
+  if ($('body').is('.jokes')) {
+    displayJoke();
+    //$('.quotes #get-quote').on('click', displayQuote);
+  }
+});
+
