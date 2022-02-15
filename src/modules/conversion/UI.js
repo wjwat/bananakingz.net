adeef77e3b4e98fb8e50534ab05eb224a35f7ded
Riley Shimp <rilryshimp@gmail.com>
2022-02-14 16:14:33 -0800

    Add code for random object API call
    Co-authored-by: Jase Seeley <jcseeley31590@yahoo.com>

--- src/modules/conversion/UI.js
+++ src/modules/conversion/UI.js
@@ -1,16 +1,14 @@
 import $ from 'jquery';
 import './conversion.css';
 import Banana from './conversion.js';
+import Random from './APIconversion.js';
 
 $(() => {
   $('#conversionForm').submit(function(event) {
     event.preventDefault();
     let unit = $('#unit').val();
-    console.log(unit);
     let input = $('#input').val();
-    console.log(input);
     let input1 = new Banana(input);
-    console.log(input1);
     if (unit === "inches"){
       input1.bananaInches();
     }
@@ -25,4 +23,27 @@ $(() => {
     }
     return $("#results").html(`${input1.bananas} BANANAS`);
   })
+
+  $("#submit").click(function(){
+    let promise = Random.getObject();
+    promise.then(function(response){
+      const body = JSON.parse(response);
+      console.log(Object.keys(body)[0]);
+      let object = Object.keys(body);
+      let weight = body[object]["weight"]/0.00272;
+      if (body[object].hasOwnProperty("length") === true) {
+        let length = body[object]["length"]/11.33333;
+        $("#randomObject").html(`Did you know ${object} is ${length} 🍌's long and weighs as much as ${weight} 🍌's!!`);
+      } else if (body[object].hasOwnProperty("height") === true) {
+        let height = body[object]['height']/11.33333;
+        $("#randomObject").html(`Did you know ${object} is ${height} 🍌's tall and weighs as much as ${weight} 🍌's!!`);
+      } else if (body[object].hasOwnProperty("circumference") === true) {
+        let circumference = body[object]['circumference']/11.33333;
+        $("#randomObject").html(`Did you know ${object} is ${circumference} 🍌's around and weighs as much as ${weight} 🍌's!!`);
+      } else {
+        let diameter = body[object]['diameter']/11.33333;
+        $("#randomObject").html(`Did you know ${object} is ${diameter} 🍌's wide and weighs as much as ${weight} 🍌's!!`);
+      }
+    });
+  });
 });
