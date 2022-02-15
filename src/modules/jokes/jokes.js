e31c4582e0d1c7b250c00ee0914c8c87efd564d2
Will W <wjwat@onionslice.org>
2022-02-15 11:41:22 -0600

    Add fetchJoke function
    Co-authored-by: Alex Adamovic <alexanderadamovic@gmail.com>

--- src/modules/jokes/jokes.js
+++ src/modules/jokes/jokes.js
@@ -1,1 +1,9 @@
+import $ from 'jquery';
+
 import './jokes.css';
+
+const fetchJoke = async () => {
+  const response = await fetch('https://wowthatsbig.net/api/randomjoke');
+
+  return response.json();
+};
