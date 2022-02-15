6a4973e1b644664da1baa92b6abbbfb03f20fc6d
Will W <wjwat@onionslice.org>
2022-02-14 18:34:59 -0600

    Fix image loading for html-loader

--- src/modules/about/about.js
+++ src/modules/about/about.js
@@ -1,1 +1,7 @@
 import './about.css';
+
+require('./assets/img/aa.jpg');
+require('./assets/img/js.jpg');
+require('./assets/img/rs.jpg');
+require('./assets/img/rb.jpg');
+require('./assets/img/ww.jpg');
