eb335f575ebcf617f99605a0aea3a6527db0fd6e
Ryan Bass <theryanbass@gmail.com>
2022-02-15 11:54:34 -0800

    add onload/buttonclick functionality

--- src/modules/facts/facts.js
+++ src/modules/facts/facts.js
@@ -1,11 +1,21 @@
 import './facts.css';
 import $ from 'jquery';
-let array = require('./facts-array.js');
+const array = require('./facts-array.js');
+
+const getFact = () => {
+  if (array.facts.length === 0) {
+    $('.facts #fact-field').text("Fact: I can't find our list of facts. That's bananas.");
+  } else if (array.facts){
+    const factNumber = Math.floor(Math.random() * array.facts.length);
+    $('.facts #fact-field').text(array.facts[factNumber]);
+  } else {
+    $('.facts #fact-field').text("Yeah, I don't know what went wrong.");
+  }
+};
 
 $(() => {
-  const randomFact = (length) => {
-    return Math.floor(Math.random() * length);
+  if ($('body').is('.facts')) {
+    $('.facts #fact-field').text(getFact);
+    $('.facts #get-fact').on('click', getFact);
   }
-  $('.test').text(array.facts[randomFact(array.facts.length)]);
 });
-
