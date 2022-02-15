2baf0a8b3b9834bb45623d9a304331a18898a4d8
Ryan Bass <theryanbass@gmail.com>
2022-02-15 15:22:26 -0800

    add bph webpackplugin

--- webpack.config.js
+++ webpack.config.js
@@ -50,6 +50,11 @@ module.exports = {
       template: "./src/modules/facts/facts.html",
       inject: "head",
       filename: "facts/index.html"
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/modules/bph/bph.html",
+      inject: "head",
+      filename: "bph/index.html"
     })
   ],
   module: {
