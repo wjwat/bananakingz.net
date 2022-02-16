f8cb14432d4ada3da318bab635c504fed5d1faf6
Jase Seeley <jcseeley31590@yahoo.com>
2022-02-15 17:09:14 -0800

    fix indentation on pong.js.

--- src/modules/pong/pong.js
+++ src/modules/pong/pong.js
@@ -4,23 +4,23 @@ import p5 from 'p5';
 
 $(() => {
   if ($('body').is('.pong')) {
-  const containerElement = document.getElementById('p5-container');
+    const containerElement = document.getElementById('p5-container');
 
-  const sketch = (p) => {
-    let x = 100;
-    let y = 100;
+    const sketch = (p) => {
+      let x = 100;
+      let y = 100;
 
-    p.setup = function() {
-      p.createCanvas(624, 351);
+      p.setup = function() {
+        p.createCanvas(624, 351);
+      };
+
+      p.draw = function() {
+        p.background(0);
+        p.fill(255);
+        p.rect(x, y, 50, 50);
+      };
     };
 
-    p.draw = function() {
-      p.background(0);
-      p.fill(255);
-      p.rect(x, y, 50, 50);
-    };
-  };
-
-  new p5(sketch, containerElement);
+    new p5(sketch, containerElement);
   }
 });
